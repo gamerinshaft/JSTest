502e1ccd1f10a80eedbc6d8580ad0b35f0d63c6a
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-27 23:02:13 +0900

    変数を使った

--- myscript.js
+++ myscript.js
@@ -1,1 +1,19 @@
-console.log("hogehoge");
+/*
+  変数: データにつけるラベル　
+  データ型
+  - 文字列
+  - 数値
+  - 真偽値(true / false)
+  - オブジェクト
+    - 配列
+    - 関数
+    - 組み込みオブジェクト
+  - undefined 定義されていないもの
+  - null 何も無い
+*/
+
+console.log("javascript mdn");
+
+var msg = "変数を使ってみたよ";
+
+console.log(msg);
