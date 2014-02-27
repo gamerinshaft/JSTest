76e6af4dbac145790dddff6c2bcdaaa23cb46f53
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 03:47:15 +0900

    domを操作してみた

--- dom.js
+++ dom.js
@@ -8,3 +8,13 @@ console.log(window.outerHeight);
 
 //window.document - 今開いているページ documentだけでもよい
 //document object model(DOM)
+
+var e = document.getElementById('msg');
+e.textContent = 'hogehoge';
+e.style.color = 'red';
+e.className = 'myStyle';
+
+var greet = document.createElement('p'),
+    text = document.createTextNode('hello!');
+document.body.appendChild(greet).appendChild(text);
+greet.className = 'text';
