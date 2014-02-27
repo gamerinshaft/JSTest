338d5d63ee510797a799698b50811fbe99e413cc
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 03:24:59 +0900

    Arrayオブジェクトについて学んだ

--- object.js
+++ object.js
@@ -38,3 +38,14 @@ var s = new String("taneichi Junpei");
 console.log(s.length);
 console.log(s.replace("t","T"));
 console.log(s.substr(1,3));
+
+var a = new Array(100, 300, 200);
+console.log(a.length);
+//unshift -> array <- push
+//shift <- array -> pop
+//splice(削除したい部分,個数)
+a.push(500);
+console.log(a);
+a.splice(1,2,200,300,400);
+console.log(a);
+
