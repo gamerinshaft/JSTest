70d874e9e126ebbf79f0002d09a2121ad18e9161
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 03:13:11 +0900

    メソッドに付いて学んだ

--- object.js
+++ object.js
@@ -12,3 +12,13 @@ console.log(user["email"]);
 console.log(user.score);
 user.score = 100;
 console.log(user);
+
+var hoge = {
+  email: "taneichi@gmail.com", //プロパティ
+  score: 80, //プロパティ
+  greet: function(name) {
+    console.log("hello," + name + " from " + this.email);
+  }//メソッド
+};
+
+hoge.greet("Tom");
