42f31d2af14cd2afffa714917aa29e80faf986aa
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-27 23:22:09 +0900

    数値を取り扱った

--- myscript.js
+++ myscript.js
@@ -10,10 +10,26 @@
     - 組み込みオブジェクト
   - undefined 定義されていないもの
   - null 何も無い
+  数値:
+    10
+    2.5
+    -2.5
+  演算子:
+    + - * / %
+    += 代入を伴う演算子
+    ++ -- 単項演算子
 */
+
 
 console.log("javascript mdn");
 
 var msg = "変数を使ってみたよ";
 
 console.log(msg);
+
+var x;
+  x = 10 * 5;
+  console.log(x);
+  x += 50;
+  console.log(x);
+  console.log("100");
