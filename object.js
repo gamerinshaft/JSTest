483c89344e8e745f5110198d9fd0088f9dc918b0
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 03:32:21 +0900

    Math,Dateオブジェクトについて学んだ

--- object.js
+++ object.js
@@ -49,3 +49,17 @@ console.log(a);
 a.splice(1,2,200,300,400);
 console.log(a);
 
+//Mathの場合newする必要がない
+console.log(Math.PI);
+console.log(Math.ceil(5.3));
+console.log(Math.floor(5.3));
+console.log(Math.round(5.3));
+console.log(Math.random());
+
+var d = new Date();
+//月は0から始まるから注意！
+//var d = new Date(2014,1,11,10,20,30);
+
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.getTime()); // 1970/1/1の経過ミリ秒
