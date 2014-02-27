e85d568812344c49492531645889d39a450ebadf
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 03:04:10 +0900

    配列、行列について学んだ

---
+++ hairetu.js
@@ -0,0 +1,21 @@
+/*
+  配列：　グループ化されたデータ
+*/
+
+var score = [100, 300, 500];
+console.log(score[0]);
+score[2] = 400;
+console.log(score);
+
+score[3] = "taneichi";
+console.log(score);
+
+
+//行列式
+var m = [
+  [1,2,3],
+  [4,5,6]
+];
+
+console.log(m[1][1]);
+console.log(m);
