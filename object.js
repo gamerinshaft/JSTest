9057739f3572d9ede496c99273d9589461285d13
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 03:09:27 +0900

    オブジェクトを使ってみた

---
+++ object.js
@@ -0,0 +1,14 @@
+/*
+  オブジェクト：
+    名前と値
+*/
+
+var user = {
+  email: "taneichi@gmail.com", //プロパティ
+  score: 80 //プロパティ
+};
+
+console.log(user["email"]);
+console.log(user.score);
+user.score = 100;
+console.log(user);
