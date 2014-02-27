569a1916435273462b74174bdfdcdb9334f66fc9
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 01:57:12 +0900

    ローカル変数と無名関数について学んだ

--- function.js
+++ function.js
@@ -4,6 +4,9 @@
   function 関数名(){
     処理
   }
+
+  関数内で定義した変数は
+  関数内で死ぬ（ローカル変数）
 */
 
 function message(msg){
@@ -13,3 +16,12 @@ function message(msg){
 function kaeriti(hoge){
   return hoge;
 }
+
+//関数の別の書き方(無名関数)
+var hello = function(name){
+  var msg = "hello" + name;
+  return msg;
+};
+
+
+
