cf27e79abc81c317e984fec8c2d855a2673616c5
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-02-28 03:40:37 +0900

    DOM とはなにかを知った

---
+++ dom.js
@@ -0,0 +1,10 @@
+/*
+  console.dirを使うと引数に用いたオブジェクトのプロパティがわかる。
+  ブラウザ自体もwindowというオブジェクト
+*/
+console.dir(window);
+console.log(window.outerHeight);
+//window.location.href = 'http://dotinstall.com'
+
+//window.document - 今開いているページ documentだけでもよい
+//document object model(DOM)
